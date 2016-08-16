1e70a1f58cddecde8950edc9ca2ec457c65efd37
liyh <lyh106415@alibaba-inc.com>
2016-08-16 18:08:59 +0800

    update with lodash.assign for node 0.12

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict'
 
 var Px2rem = require('px2rem')
+var assign = require('lodash.assign')
+
 var defaultOptions = {
     remUnit: 75,
     threeVersion: false,
@@ -10,7 +12,7 @@ var defaultOptions = {
 }
 
 module.exports = function(content, file, conf) {
-    var options = Object.assign({}, defaultOptions, conf)
+    var options = assign({}, defaultOptions, conf)
     var px2rem = new Px2rem(options)
     return px2rem.generateRem(content)
 }
